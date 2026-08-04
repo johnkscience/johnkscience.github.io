09e1ac93626e80af7d54d08971f5b900a20a722c
johnkscience <johnkoum1@yahoo.gr>
2026-08-04 13:00:50 +0300

    Μεταφορά νέου site και ενημέρωση αρχείων

---
+++ notes/math_ag_content/javascripts/mathjax.js
@@ -0,0 +1,28 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  },
+  startup: {
+    ready: () => {
+      // Αρχικοποίηση του MathJax
+      MathJax.startup.defaultReady();
+      
+      // Προσθήκη φίλτρου πριν το rendering
+      MathJax.startup.document.inputJax.forEach(jax => {
+        if (jax.name === 'TeX') {
+          jax.preFilters.add((args) => {
+            // Αντικαθιστά κάθε σχήμα "ψηφίο,ψηφίο" (π.χ. 2,3) με "ψηφίο{,}ψηφίο" (2{,}3)
+            args.math.math = args.math.math.replace(/(\d),(\d)/g, '$1{,}$2');
+          });
+        }
+      });
+    }
+  }
+};
--- share/lab/files/pendulum/lib01/konva/index.js
+++
@@ -1,8 +0,0 @@
-// konva/index.js
-import Konva from 'konva'; // Υποθέτουμε ότι το konva.js είναι στο ίδιο φάκελο
-
-// Εξάγουμε τις κλάσεις και τις λειτουργίες που θέλουμε να είναι διαθέσιμες
-export { Konva };
-
-// Ή, αν θέλουμε να εξάγουμε συγκεκριμένες κλάσεις:
-// export { Konva, Konva.Rect, Konva.Circle, ... };
